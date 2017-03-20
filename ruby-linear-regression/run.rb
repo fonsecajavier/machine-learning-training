68e1ca122cb709011cccc3c465b9729888e4ddca
Javier Fonseca V <fonsecajavier@gmail.com>
2017-03-20 07:31:34 -0500

    Reordering some little logic

--- ruby-linear-regression/run.rb
+++ ruby-linear-regression/run.rb
@@ -5,16 +5,13 @@ def do_linear_regression(samples:, alpha:, theta0:, theta1:, precision:, delay: 
   max_error = 1.0 / (10 ** precision)
   iterations = 0
   while true do
-    puts "Doing theta0: #{theta0} and theta1: #{theta1} for iteration #{iterations + 1}"
+    iterations += 1
+    puts "Doing theta0: #{theta0} and theta1: #{theta1} for iteration #{iterations}"
     temp0 = theta0 - alpha * (1.0 / m) * cost_derivative_theta0(theta0, theta1, samples)
     temp1 = theta1 - alpha * (1.0 / m) * cost_derivative_theta1(theta0, theta1, samples)
-    if (temp0 - theta0).abs < max_error && (temp1 - theta1).abs < max_error
-      break
-    else
-      theta0 = temp0
-      theta1 = temp1
-    end
-    iterations += 1
+    break if (temp0 - theta0).abs < max_error && (temp1 - theta1).abs < max_error
+    theta0 = temp0
+    theta1 = temp1
     sleep delay
   end
   puts "Found proper approximate for the requested precision after #{iterations} iterations:"
